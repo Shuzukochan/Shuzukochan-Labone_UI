be8d7f7bb620369a6b4740a05efb20f049724c9b
Shuzuko <shuzukochan@gmail.com>
2024-12-09 09:59:45 +0700

    9h59

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -153,29 +153,9 @@ namespace Labone_UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap DataLogger {
-            get {
-                object obj = ResourceManager.GetObject("DataLogger", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap direction {
             get {
                 object obj = ResourceManager.GetObject("direction", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Edit {
-            get {
-                object obj = ResourceManager.GetObject("Edit", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
